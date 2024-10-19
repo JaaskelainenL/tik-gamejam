df32fc2b336c0d364b584582b22807599f79bc66
oskarihied <oskari.hiedanpaa@aalto.fi>
2024-10-19 17:51:11 +0300

    added enemy but it broken :(

---
+++ scenes/enemy.gd
@@ -0,0 +1,40 @@
+# File: enemy.gd
+extends CharacterBody3D
+
+# Constants
+const GRAVITY: float = -9.8
+const PUSH_FORCE: float = 10.0
+const MAX_FALL_SPEED: float = -20.0
+const DECELERATION: float = 2.0
+var HIT_TIMER: float = 0.0
+const HIT_COOLDOWN: float = 0.5
+
+# Called when the physics engine updates (every physics frame)
+func _physics_process(delta: float) -> void:
+	# Apply gravity
+	'''if not is_on_floor():
+		velocity.y += GRAVITY * delta
+		velocity.y = max(velocity.y, MAX_FALL_SPEED)  # Limit fall speed
+	else:
+		velocity.y = 0  # Reset Y velocity when on the floor
+	'''
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * max(velocity.length() - DECELERATION * delta, 0)
+	# Move and slide along surfaces (use move_and_slide with no arguments)
+	move_and_slide()
+	HIT_TIMER += delta
+	print(str(HIT_TIMER))
+	# Check for collisions with other bodies
+
+	for i in range(get_slide_collision_count()):
+		var collision = get_slide_collision(i)
+		var collider = collision.get_collider()
+		
+		if collider.name != "floor":
+			print("hit")
+			if HIT_TIMER > HIT_COOLDOWN:
+				# Apply push force based on collision normal
+				
+				var push_direction = collision.get_normal() * -PUSH_FORCE
+				velocity += push_direction
+				HIT_TIMER = 0.0
--- scenes/player.gd
+++ scenes/player.gd
@@ -8,14 +8,15 @@ var doing_action = false
 var roulette_active = false
 var roulette_instance = null
 
+# Movement speed on the ground
+var speed: float = 5.0  # You can adjust this value
 
 func _process(delta: float) -> void:
-
 	if is_on_floor() and !doing_action and !roulette_active:
 		rotate_y(SPIN * delta)
 
 func _physics_process(delta: float) -> void:
-	#
+	# Player movement and jump logic
 	if Input.is_action_just_pressed("ui_accept"):
 		if !doing_action and !roulette_active and is_on_floor():
 			doing_action = true
@@ -23,26 +24,40 @@ func _physics_process(delta: float) -> void:
 				var roulette_scene = load("res://scenes/ruletti.tscn") as PackedScene
 				if roulette_scene:
 					roulette_instance = roulette_scene.instantiate()
-					# This is how you add extra options to the roulette
-					#roulette_instance.get_node("rulettitausta/Sprite3D").add_option("test")
-
 					var camera_node = get_tree().root.get_node("Game/Pleijeri/Camera3D")
 					if camera_node:
 						camera_node.add_child(roulette_instance)
 						roulette_instance.name = "Roulette"
-
 			
 			roulette_active = true
 		elif roulette_active:
 			roulette_active = false
 			$suuntaNuoli.visible = true
 
+	# Gravity handling
 	if !is_on_floor():
 		velocity += get_gravity() * delta
 	elif is_on_floor() and !doing_action and velocity.y == 0:
-		velocity.x = 0.0
-		velocity.z = 0.0
+		# Horizontal movement logic (apply speed when on the floor)
+		var input_direction = Vector3.ZERO
+		if Input.is_action_pressed("ui_left"):
+			input_direction.x -= 1
+		if Input.is_action_pressed("ui_right"):
+			input_direction.x += 1
+		if Input.is_action_pressed("ui_up"):
+			input_direction.z -= 1
+		if Input.is_action_pressed("ui_down"):
+			input_direction.z += 1
 
+		if input_direction != Vector3.ZERO:
+			input_direction = input_direction.normalized()
+			velocity.x = input_direction.x * speed
+			velocity.z = input_direction.z * speed
+		else:
+			velocity.x = 0.0
+			velocity.z = 0.0
+
+	# Apply movement
 	move_and_slide()
 
 func roulette_action(action):
