dc659ff246f32daa44445532ae07eb18189a6b6a
jaaskelainenL <jaaskelainenL@protonmail.com>
2024-10-19 18:49:04 +0300

    duct tape fix the collision

--- scenes/enemy.gd
+++ scenes/enemy.gd
@@ -12,18 +12,18 @@ const HIT_COOLDOWN: float = 0.5
 # Called when the physics engine updates (every physics frame)
 func _physics_process(delta: float) -> void:
 	# Apply gravity
-	'''if not is_on_floor():
+	if not is_on_floor():
 		velocity.y += GRAVITY * delta
 		velocity.y = max(velocity.y, MAX_FALL_SPEED)  # Limit fall speed
 	else:
+		position.y += 0.001
 		velocity.y = 0  # Reset Y velocity when on the floor
-	'''
+		
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * max(velocity.length() - DECELERATION * delta, 0)
 	# Move and slide along surfaces (use move_and_slide with no arguments)
 	move_and_slide()
 	HIT_TIMER += delta
-	print(str(HIT_TIMER))
 	# Check for collisions with other bodies
 
 	for i in range(get_slide_collision_count()):
@@ -34,7 +34,7 @@ func _physics_process(delta: float) -> void:
 			print("hit")
 			if HIT_TIMER > HIT_COOLDOWN:
 				# Apply push force based on collision normal
+				var push_direction = collision.get_normal() * PUSH_FORCE
 				
-				var push_direction = collision.get_normal() * -PUSH_FORCE
 				velocity += push_direction
 				HIT_TIMER = 0.0
